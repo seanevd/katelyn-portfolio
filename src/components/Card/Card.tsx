dade3afccdf01bb5be6d5d396386b1d01e25ac3d
seanevd <seaneduncan@gmail.com>
2024-07-27 16:14:46 -0700

    added about page and other misc style updates

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -21,29 +21,33 @@ export default function Card({
     return (
         <div className="link-card">
             <a href={href}>
-                <ul className="company-images">
-                    {companyImages.map((img, i) => (
-                        <li key={i}>
-                            <img
-                                src={`/project-images/${slug}/logos/${img}`}
-                                alt={`${title} logo`} />
-                        </li>
-                    ))}
-                </ul>
-                <h2 className="title-card-text">
-                    {title}
-                </h2>
-                <p className="summary-text">
-                    {summary}
-                </p>
-                <div className="arrow-elem">
-                    <div className="arrow"></div>
+                <div className="top">
+                    <ul className="company-images">
+                        {companyImages.map((img, i) => (
+                            <li key={i}>
+                                <img
+                                    src={`/project-images/${slug}/logos/${img}`}
+                                    alt={`${title} logo`} />
+                            </li>
+                        ))}
+                    </ul>
+                    <h2 className="title-card-text">
+                        {title}
+                    </h2>
+                    <p className="summary-text">
+                        {summary}
+                    </p>
+                    <div className="arrow-elem">
+                        <div className="arrow"></div>
+                    </div>
                 </div>
-                <ul className="tag-list">
-                    {tags?.map((tag, i) => (
-                        <li className="mono-bold" key={i}>{tag}</li>
-                    ))}
-                </ul>
+                <div className="bottom">
+                    <ul className="tag-list">
+                        {tags?.map((tag, i) => (
+                            <li className="mono-bold" key={i}>{tag}</li>
+                        ))}
+                    </ul>
+                </div>
             </a>
         </div>
     )
--- src/consts.ts
+++ src/consts.ts
@@ -2,6 +2,13 @@ export const SITE_TITLE = 'Katelyn Duncan';
 export const SITE_MOTTO = 'Design is my job';
 export const SITE_DESCRIPTION = 'I am a product designer with over 7 years of experience designing and building digitally-focused products and services. I am currently researching and desigining digital tools to support the Artemis Moon program at NASA Ames Research Center in Mountain View, California.';
 export const EMAIL_ADDRESS = 'duncan.katelynj@gmail.com'
+export const KATELYN_TAGS = [
+    "Designer",
+    "Baker",
+    "Runner",
+    "Explorer",
+    "Swimmer"
+]
 
 export const SITE_TAGS = [
     "Product Design",
